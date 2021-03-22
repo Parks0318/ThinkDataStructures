59ca3c16525a665d7b2888f226add54be070bd28
Sohyeon park <sohyeonpark@yanolja.com>
2021-03-22 22:11:32 +0900

    3 - MyLinkedList

---
+++ intellij/code/src/test/java/MyLinkedListTest.java
@@ -0,0 +1,18 @@
+import com.allendowney.thinkdast.MyArrayListTest;
+import com.allendowney.thinkdast.MyLinkedList;
+import org.junit.Before;
+
+import java.util.ArrayList;
+
+public class MyLinkedListTest extends MyArrayListTest {
+    @Before
+    public void setUp() throws Exception {
+        list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        mylist = new MyLinkedList<Integer>();
+        mylist.addAll(list);
+    }
+}
